4b51a8699f9b71331b47157b8a88206011899865
Aksel Henrik Lessum <31306197+AkselLessum@users.noreply.github.com>
2021-11-07 18:02:50 +0100

    Gjort slik at menyen legges til via javascript, mangler alle rettene

--- Website/scripts/dropDownMenu.js
+++ Website/scripts/dropDownMenu.js
@@ -2,13 +2,13 @@
 const menuDropdown = document.createElement("div");
 menuDropdown.className = "dropDown"
 const dropdown1 = document.createElement("a")
-dropdown1.href = "footer"
+dropdown1.href = ".footer"
 dropdown1.innerHTML = "Footer"
 const dropdown2 = document.createElement("a")
-dropdown2.href = "footer"
+dropdown2.href = ".footer"
 dropdown2.innerHTML = "Footer"
 const dropdown3 = document.createElement("a")
-dropdown3.href = "footer"
+dropdown3.href = ".footer"
 dropdown3.innerHTML = "Footer"
 menuLink.appendChild(menuDropdown)
 menuDropdown.appendChild(dropdown1)
@@ -17,5 +17,5 @@ menuDropdown.appendChild(dropdown3)
 
 menuLink.onmouseover = function(){
     menuLink.style.backgroundColor = "lightgrey";
-    menuDropdown.style.display = "block"
+    menuDropdown.style.display = "block";
 }
