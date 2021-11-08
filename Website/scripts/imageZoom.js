055330cf3a79fabb0f05662db9b166fe0b6d9f94
Jens Waage <waage.jens@gmail.com>
2021-11-08 18:17:23 +0100

    Add basic zoom handler

---
+++ Website/scripts/imageZoom.js
@@ -0,0 +1,19 @@
+function zoomImage(event) {
+    const clickedImage = event.currentTarget;
+    const zoomedImage = document.createElement("img");
+    zoomedImage.src = clickedImage.src;
+    zoomedImage.classList.add("zoomed-image");
+    zoomedImage.id = clickedImage.id + "-zoom";
+    const zoomBackground = document.createElement("div");
+    zoomBackground.classList.add("zoom-background");
+    zoomBackground.appendChild(zoomedImage);
+
+    document.body.appendChild(zoomBackground);
+
+    for (i = 1; i <= 1.5; i+= 0.1) {
+        zoomedImage.style.transform = "scale(" + String(i) + ")";
+    }
+
+
+
+}
