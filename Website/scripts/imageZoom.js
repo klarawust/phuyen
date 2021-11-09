82540370ab094f932c3a615cf8dc987d832dbd33
Jens Waage <waage.jens@gmail.com>
2021-11-09 16:48:14 +0100

    Add zoom out functionality on click

--- Website/scripts/imageZoom.js
+++ Website/scripts/imageZoom.js
@@ -1,14 +1,18 @@
+const zoomBackground = document.createElement("div");
+zoomBackground.classList.add("zoom-background");
+document.body.appendChild(zoomBackground);
+
 function zoomIn(event) {
     const clickedImage = event.currentTarget;
+    
     const zoomedImage = document.createElement("img");
     zoomedImage.src = clickedImage.src;
     zoomedImage.classList.add("zoomed-image");
     zoomedImage.id = clickedImage.id + "-zoom";
-    const zoomBackground = document.createElement("div");
-    zoomBackground.classList.add("zoom-background");
-    zoomBackground.appendChild(zoomedImage);
+    zoomedImage.addEventListener("click", zoomOut);
 
-    document.body.appendChild(zoomBackground);
+    zoomBackground.style.display = "flex";
+    zoomBackground.appendChild(zoomedImage);
     window.removeEventListener("scroll", setStickyHeader); // remove listener
 
     for (i = 1; i <= 1.5; i+= 0.1) {
@@ -16,6 +20,9 @@ function zoomIn(event) {
     }
 }
 
-function zoomOut() {
-    
+function zoomOut(event) {
+    const zoomedImage = event.currentTarget;
+    zoomBackground.style.display = "none";
+    zoomBackground.removeChild(zoomedImage);
+    window.addEventListener("scroll", setStickyHeader);
 }
