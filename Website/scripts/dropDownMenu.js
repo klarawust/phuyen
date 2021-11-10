484878dacda7746ce0b7a94735761709152e69ed
Aksel Henrik Lessum <akselhl@ntnu.no>
2021-11-10 11:16:42 +0100

    Små y-verdi endringer

--- Website/scripts/dropDownMenu.js
+++ Website/scripts/dropDownMenu.js
@@ -33,6 +33,9 @@ menuLink.onmouseover = function(){
 headerDiv.onmouseleave = function(){
     menuDropdown.style.display = "none";
 }
-window.addEventListener("hashchange", function () {/*Noe kode lånt fra stackoverflow*/
+
+/*Kode hentet fra stack overflow, brukes for å offsette det at headeren dekker over for referansen*/
+/*https://stackoverflow.com/questions/17534661/make-anchor-link-go-some-pixels-above-where-its-linked-to*/
+window.addEventListener("hashchange", function () {
     window.scrollTo(window.scrollX, window.scrollY - 130);
 });
