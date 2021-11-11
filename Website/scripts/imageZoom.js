959d407c98727b35c4b6c8da06b30d859a80a30c
Jens <waage.jens@gmail.com>
2021-11-11 15:48:35 +0100

    Code cleanup and commenting

--- Website/scripts/imageZoom.js
+++ Website/scripts/imageZoom.js
@@ -1,16 +1,4 @@
-const zoomBackground = document.createElement("div");
-zoomBackground.classList.add("zoom-background");
-zoomBackground.addEventListener("click", zoomOut);
-document.body.appendChild(zoomBackground);
-document.addEventListener("keydown", zoomOut)
-
-const images = document.getElementsByTagName("img");
-for(i = 0; i < images.length; i++) {
-    if (images[i].id != "logo") {
-        images[i].addEventListener("click", zoomIn);
-    }
-}
-
+/**event handler when image is clicked */
 function zoomIn(event) {
     const clickedImage = event.currentTarget;
     
@@ -18,21 +6,35 @@ function zoomIn(event) {
     zoomedImage.src = clickedImage.src;
     zoomedImage.classList.add("zoomed-image");
     zoomedImage.id = clickedImage.id + "-zoom";
-    //zoomedImage.addEventListener("click", zoomOut);
 
     zoomBackground.style.display = "flex";
     
     zoomBackground.appendChild(zoomedImage);
 
-    for (i = 1; i <= 1.5; i+= 0.1) {
-        zoomedImage.style.transform = "scale(" + String(i) + ")";
+    zoomedImage.style.transform = "scale(1.5)"; // enhance image
+}
+
+/**Zoom out event handler triggered either by clicking on background or hitting escape*/
+function zoomOut(event) {
+    if (event.type == "click" || event.key == "Escape") {
+        zoomBackground.style.display = "none"; // hide background
+        zoomBackground.innerHTML = ""; // clear image
     }
 }
 
-function zoomOut(event) {
-    if (event.type == "click" || event.key == "Escape") {
-        zoomBackground.style.display = "none";
-        zoomBackground.innerHTML = "";
+/**Setup black opaque background when image is clicked */
+const zoomBackground = document.createElement("div");
+zoomBackground.classList.add("zoom-background");
+zoomBackground.addEventListener("click", zoomOut);
+document.body.appendChild(zoomBackground);
+document.addEventListener("keydown", zoomOut)
+
+/**Add zoomIn event handler to all images on page except logo in header*/
+const images = document.getElementsByTagName("img");
+for(i = 0; i < images.length; i++) {
+    if (images[i].id != "logo") {
+        images[i].addEventListener("click", zoomIn);
     }
 }
+
 
